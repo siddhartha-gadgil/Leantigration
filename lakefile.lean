cdef14c0945d4061c4dd2d81f925004cd42d65c8
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-03-07 08:33:35 +0530

    update to v4.6.1

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ package «Leantegration» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.6.1"
 
 @[default_target]
 lean_lib «Leantegration» where
